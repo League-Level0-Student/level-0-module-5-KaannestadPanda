7db70f856c498df59c5792e2540550d8a73e7bfe
KaannestadPanda <37317621+KaannestadPanda@users.noreply.github.com>
2018-06-27 12:00:28 -0700

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Prime {
+public static void main(String[] args) {
+	String flop=JOptionPane.showInputDialog("number?");
+int ha=Integer.parseInt(flop);
+	for(int g=ha;g>0;g--) {
+		JOptionPane.showMessageDialog(null, ha%g);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+}
