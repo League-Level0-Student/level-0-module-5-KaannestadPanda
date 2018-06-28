4cd28c7684d94cd064bece0c26b6c097f3ee7bb6
League <league@WTS9.attlocal.net>
2018-06-28 14:58:52 -0700

    forloopgauntlet
    did a lot on the gauntlet

---
+++ src/forloopgauntlet.java
@@ -0,0 +1,106 @@
+
+public class forloopgauntlet {
+
+	public static void main(String[] args) {
+		
+//	//1 
+//		for(int i=0;i<101;i++) {
+//		System.out.println(i);
+//	}
+//	
+//		//2
+//	for(int f=100;f>-1;f--) {
+//		System.out.println(f);
+//	}
+//	
+//	//3
+//	for(int g=2;g<101;g++) {
+//		if(g%2==0) {
+//			System.out.println(g);
+//		}
+//	}
+//	
+//		for(int g=1;g<100;g++) {
+//			if(g%2!=0) {
+//				System.out.println(g);
+//			}
+//		}
+	
+//		for(int g=1;g<501;g++) {
+//			if(g%2!=0) {
+//				System.out.println(g+" is odd");
+//			}
+//			else if(g%2==0) {
+//				System.out.println(g+" is even");
+//			}
+		
+//		for(int g=1;g<778;g++) {
+//		if(g%7==0) {
+//			System.out.println(g);
+//		}
+//		
+//	
+//		}
+//	int old=1;
+//	System.out.println("In 2004, I was 0 years old.");
+//		for(int g=2005;g<2018;g++) {
+//			System.out.println("In "+g+", I was "+old+" years old.");
+//			old++;
+//		}
+	
+//	for(int g=0;g<3;g++) {
+//		for(int i=0; i<3;i++) {
+//			System.out.println(g+" "+i);
+//		}
+//		
+//	}
+		
+		
+		
+//	int n=1;
+//		for(int g=0;g<3;g++) {
+//			for(int j=n; j<n+3;j++) {
+//				
+//				System.out.print(j+" ");
+//				
+//			}
+//			n+=3;
+//			System.out.println();
+//		}
+		
+		
+		
+//		int n=1;
+//		for(int g=0;g<10;g++) {
+//			for(int j=n; j<n+10;j++) {
+//				if(j<10) {
+//					System.out.print("0"+j+" ");
+//				}
+//				else {
+//				System.out.print(j+" ");
+//				}
+//			}
+//			n+=10;
+//			System.out.println();
+//		}
+		
+		
+		int n=1;
+		for(int g=0;g<6;g++) {
+			for(int j=n; j<n+n;j++) {
+				
+					System.out.print("* ");
+				
+				
+			}
+			n+=1;
+			System.out.println();
+		}
+		
+		
+		
+	
+	
+	
+	}
+}
