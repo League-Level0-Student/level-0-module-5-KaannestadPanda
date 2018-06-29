00c581445d34c66758bd5f6327344ad040881d6b
KaannestadPanda <league@WTS-iMac-11.attlocal.net>
2018-06-29 14:56:27 -0700

    circling,evilpolygons
    did polygons, added stuff to circle ring

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,52 +1,62 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
+		System.out.println("hi");
+		// 1. Make a new Robot
+		Random gen = new Random();
+		int ja = 0;
+		
+		Robot Rob = new Robot();
+		Rob.setX(150);
+		Rob.setY(200);
+		Rob.setRandomPenColor();
+		Rob.setPenWidth(ja);
+		Rob.penDown();
+		Rob.setSpeed(500);
+		for (int i = 0; i < 360; i++) {
+			Rob.setRandomPenColor();
+			//ja = gen.nextInt(7000);
+			Rob.setPenWidth(7000000);
+			Rob.move(3);
+			Rob.turn(1);
+			if (i % 20 == 0) {
+				for (int j = 0; j < 360; j++) {
+					Rob.setRandomPenColor();
+					//ja = gen.nextInt(7000);
+					Rob.setPenWidth(7000000);
+					Rob.move(1);
+					Rob.turn(1);
+				}
+			}
 
-        // 1. Make a new Robot
-
-Robot Rob=new Robot();
-Rob.setX(150);
-Rob.setY(200);
-Rob.penDown();
-Rob.setSpeed(500);
-for(int i=0;i<360;i++) {
-	Rob.move(3);
-	Rob.turn(1);
-	if(i%20==0) {
-		for(int j=0;j<360;j++) {
-	Rob.move(1);
-	Rob.turn(1);
 		}
-	}
-	
-	
-	
-	
-}
-       //  2. Set your robot’s position to x=150 and y=200
+		// 2. Set your robot’s position to x=150 and y=200
 
-        // 3. Put the robot's pen down
+		// 3. Put the robot's pen down
 
-        // 4. Set the robot’s speed to 10
+		// 4. Set the robot’s speed to 10
 
-        // 5. Do everything below here 360 times (use i as the counter)
+		// 5. Do everything below here 360 times (use i as the counter)
 
-	        // 6. Move the robot 3 pixels
+		// 6. Move the robot 3 pixels
 
-       //  7. Turn the robot 1 degree
+		// 7. Turn the robot 1 degree
 
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
@@ -9,13 +10,38 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot dank=new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+dank.setSpeed(5000);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"red","green", "blue"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
+		if(colorChoice==0) {
+			dank.setPenColor(255,0,0);
+		}
+		else if(colorChoice==1) {
+			dank.setPenColor(0,255,0);
+		}
+		else if(colorChoice==2) {
+			dank.setPenColor(0,0,255);
+		}
+		dank.penDown();
+		String t=JOptionPane.showInputDialog("how many polygons?");
+		int g=Integer.parseInt(t);
+		for(int n=0;n<g;n++) {
+			dank.penDown();
+			for(int r=0;r<4;r++) {
+				dank.move(20);
+				dank.turn(90);
+			}
+			dank.penUp();
+			Random gen=new Random();
+			int q=gen.nextInt(181)-90;
+			dank.turn(q);
+		dank.move(50);
 		
+		}
+		dank.move(500);
 		//4. Ask the use how many polygons they want to be drawn.
 		
 		//5. Use the robot to draw the number of polygons the user requested.
